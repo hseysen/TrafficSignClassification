6ff2d54f5623d05a9040937aef5a33259e5c3d5e
hseysen <hesenisgenderli999@gmail.com>
2024-03-29 16:56:32 +0400

    started writing splitter and augmentator

---
+++ datasets/dataset_augmentate.py
@@ -0,0 +1,28 @@
+import os
+import random
+from PIL import Image, ImageOps
+
+
+class DataAugmentator:
+    def __init__(self):
+        self.root = "datasets/TrafficSign"
+        self.datadir = os.path.join(self.root, "DATA")
+
+    def rotate_image(self, image, rotation_range=(-30, 30)):
+        angle = random.randint(rotation_range[0], rotation_range[1])
+        return image.rotate(angle)
+
+    def scale_image(self, image, scale_range=(0.75, 1.25)):
+        scale_factor = random.uniform(scale_range[0], scale_range[1])
+        width, height = image.size
+        new_width = int(width * scale_factor)
+        new_height = int(height * scale_factor)
+        return image.resize((new_width, new_height))
+
+    def save_transformed_image(self, image, filename):
+        image.save(filename)
+
+
+if __name__ == "__main__":
+    print("unimplemented")
+    
---
+++ datasets/dataset_split.py
@@ -0,0 +1,17 @@
+import os
+
+
+class DataSplitter:
+    def __init__(self):
+        self.root = "datasets/TrafficSign"
+        self.datadir = os.path.join(self.root, "DATA")
+
+    def view_instances(self):
+        for cls in os.listdir(self.datadir):
+            clsdir = os.path.join(self.datadir, cls)
+            print(len(os.listdir(clsdir)), "instances for", cls)
+
+
+if __name__ == "__main__":
+    DataSplitter().view_instances()
+    
