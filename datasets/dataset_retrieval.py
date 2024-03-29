3c9a56f1dee9e47b0bb70133f9bd62637927dea4
hseysen <hesenisgenderli999@gmail.com>
2024-03-29 18:38:38 +0400

    general nitpicking

--- datasets/dataset_retrieval.py
+++ datasets/dataset_retrieval.py
@@ -1,52 +1,49 @@
-import torch 
-import torch.nn
-from PIL import Image
-from torchvision import transforms
 import os
 import numpy as np
+from PIL import Image
+import torch
+import torch.nn
 from torch.utils.data import Dataset
 
 
-
 class custom_dataset(Dataset):
-    def __init__(self, mode = "train", root = "datasets/demo_dataset", transforms = None):
+    def __init__(self, mode="train", root="datasets/demo_dataset", transforms=None):
         super().__init__()
         self.mode = mode
         self.root = root
         self.transforms = transforms
-        
-        #select split
+
+        # Select split
         self.folder = os.path.join(self.root, self.mode)
-        
-        #initialize lists
+
+        # Initialize lists
         self.image_list = []
         self.label_list = []
-        
-        #save class lists
+
+        # Save class lists
         self.class_list = os.listdir(self.folder)
         self.class_list.sort()
-        
+
+        # Loop over every image and add the corresponding entry to image and label lists
         for class_id in range(len(self.class_list)):
             for image in os.listdir(os.path.join(self.folder, self.class_list[class_id])):
                 self.image_list.append(os.path.join(self.folder, self.class_list[class_id], image))
                 label = np.zeros(len(self.class_list))
                 label[class_id] = 1.0
                 self.label_list.append(label)
-        
+
     def __getitem__(self, index):
+        # Retrieve the entry at requested index
         image_name = self.image_list[index]
         label = self.label_list[index]
-        
-        
+
+        # Convert label to a tensor, and apply transforms to the image
+        label = torch.tensor(label)
         image = Image.open(image_name)
         if(self.transforms):
             image = self.transforms(image)
-        
-        label = torch.tensor(label)
-        
+
         return image, label
-            
+
     def __len__(self):
-        return len(self.image_list)        
-
-
+        return len(self.image_list)
