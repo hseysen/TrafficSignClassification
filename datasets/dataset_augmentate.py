cf285ac98043357d80827ceeff32e069187c7b4d
hseysen <hesenisgenderli999@gmail.com>
2024-03-31 16:44:51 +0400

    Removed scale augmentation, as it is not necessary

--- datasets/dataset_augmentate.py
+++ datasets/dataset_augmentate.py
@@ -12,13 +12,6 @@ class DataAugmentator:
         angle = random.randint(rotation_range[0], rotation_range[1])
         return image.rotate(angle)
 
-    def scale_image(self, image, scale_range=(0.75, 1.25)):
-        scale_factor = random.uniform(scale_range[0], scale_range[1])
-        width, height = image.size
-        new_width = int(width * scale_factor)
-        new_height = int(height * scale_factor)
-        return image.resize((new_width, new_height))
-
     def save_transformed_image(self, image, filename):
         image.save(filename)
     
@@ -30,11 +23,6 @@ class DataAugmentator:
         # Test rotation
         result1 = self.rotate_image(test_image)
         result1.save("tmp/rotation_augmentation.png")
-
-        # Test Scaling
-        result2 = self.scale_image(test_image)
-        result2.save("tmp/scale_augmentation.png")
-
 
 
 if __name__ == "__main__":
