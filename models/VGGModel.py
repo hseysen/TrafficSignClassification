5fbc7ad3f141e5d39222e834c0c6c28daab7f316
hseysen <hesenisgenderli999@gmail.com>
2024-04-01 15:47:46 +0400

    currently vgg does not learn

--- models/VGGModel.py
+++ models/VGGModel.py
@@ -4,13 +4,13 @@ import torchvision.models as models
 class VGG16(nn.Module):
     def __init__(self, class_count):
         super().__init__()
-        self.vgg16 = models.vgg16(pretrained=True)
+        self.vgg16 = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
 
         # Freeze parameters
         for param in self.vgg16.parameters():
             param.requires_grad = False
 
-        self.vgg16.classifier[6] = nn.Linear(self.vgg16.classifier[6].in_features, class_count)
+        self.vgg16.classifier[-1] = nn.Linear(self.vgg16.classifier[-1].in_features, class_count)
 
     def forward(self, image):
         return self.vgg16(image)
--- train.py
+++ train.py
@@ -1,65 +1,58 @@
+import os
+import tqdm
+from utils import retrieve_norms
 import torch
 import torch.nn as nn
-from torch.nn.functional import softmax
-from torch.nn.functional import cross_entropy
 from torchmetrics import F1Score
 from torchvision import transforms
-
-from torch.utils.data import DataLoader, Dataset
-from models.example_model import ExModel
-from datasets.dataset_retrieval import custom_dataset
+from torch.utils.data import DataLoader
 from torch.optim import SGD, Adam
+from datasets.dataset_retrieval import TrafficSignDataset
+from models.ResnetModel import Resnet
+from models.VGGModel import VGG16
 from torch.utils.tensorboard import SummaryWriter
-import tqdm
-
-import os
-
-save_model_path = "checkpoints/"
-pth_name = "saved_model.pth"
 
 
+MODEL_PATH = "checkpoints/"
+PTH_NAME = f"model_vgg16_sgd.pth"
 
 
 def val(model, data_val, loss_function, writer, epoch, device):
     f1score = 0
-    f1 = F1Score(num_classes=53, task = 'multiclass')
-    data_iterator = enumerate(data_val)  # take batches
+    f1 = F1Score(num_classes=54, task="multiclass")
+    data_iterator = enumerate(data_val)
     f1_list = []
     f1t_list = []
 
     with torch.no_grad():
-        model.eval()  # switch model to evaluation mode
+        model.eval()
         tq = tqdm.tqdm(total=len(data_val))
-        tq.set_description('Validation:')
+        tq.set_description("Validation:")
 
         total_loss = 0
-
         for _, batch in data_iterator:
-            # forward propagation
+            # Forward propagation
             image, label = batch
             image = image.to(device)
             label = label.to(device)
             pred = model(image)
-
+            
+            # Compute loss
             loss = loss_function(pred, label.float())
-
             pred = pred.softmax(dim=1)
             
-            f1_list.extend(torch.argmax(pred, dim =1).tolist())
-            f1t_list.extend(torch.argmax(label, dim =1).tolist())
-
+            f1_list.extend(torch.argmax(pred, dim=1).tolist())
+            f1t_list.extend(torch.argmax(label, dim=1).tolist())
+            
             total_loss += loss.item()
             tq.update(1)
 
-    
     f1score = f1(torch.tensor(f1_list), torch.tensor(f1t_list))
     writer.add_scalar("Validation F1", f1score, epoch)
-    writer.add_scalar("Validation Loss", total_loss/len(data_val), epoch)
-
+    writer.add_scalar("Validation Loss", total_loss / len(data_val), epoch)
 
     tq.close()
-    print("F1 score: ", f1score)
-
+    print(f"F1 score: {f1score}")
 
     return None
 
@@ -67,22 +60,18 @@ def val(model, data_val, loss_function, writer, epoch, device):
 def train(model, train_loader, val_loader, optimizer, loss_fn, n_epochs, device):
     writer = SummaryWriter()
 
-    model.to(device)  # Move the model to the specified device (e.g., GPU or CPU)
-    model.train()  # Set the model to training mode
+    model.to(device)
+    model.train()
     for epoch in range(n_epochs):
-        
-        
         model.train()
-        running_loss = 0.0
-        
         tq = tqdm.tqdm(total=len(train_loader))
-        tq.set_description('epoch %d' % (epoch))
-        
+        tq.set_description(f"Epoch #{epoch}")
+
+        running_loss = 0.0
         for i, (images, labels) in enumerate(train_loader):
-            images = images.to(device)  # Move the batch of images to the specified device
-            labels = labels.to(device)  # Move the batch of labels to the specified device
-            
-            optimizer.zero_grad()  # Reset the gradients of the optimizer
+            images = images.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
             
             # Forward pass
             outputs = model(images)
@@ -98,62 +87,53 @@ def train(model, train_loader, val_loader, optimizer, loss_fn, n_epochs, device)
             optimizer.step()
             
             running_loss += loss.item()
-            tq.set_postfix(loss_st='%.6f' % loss.item())
+            tq.set_postfix(loss_st=f"{loss.item():.6f}")
             tq.update(1)
         
-        writer.add_scalar("Training Loss", running_loss/len(train_loader), epoch)
-           
+        writer.add_scalar(f"Training Loss", running_loss / len(train_loader), epoch)
+        
         tq.close()
         epoch_loss = running_loss / len(train_loader)
-        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, n_epochs, epoch_loss))
-                
-        #check the performance of the model on unseen dataset4
+        print(f"Epoch [{epoch + 1} / {n_epochs}], Loss: {epoch_loss:.4f}")
+        
+        # Check the performance of the model on unseen dataset
         val(model, val_loader, loss_fn, writer, epoch, device)
         
-        #save the model in pth format
+        # Save the model in ".pth" format
         checkpoint = {
-            'epoch': epoch + 1,
-            'state_dict': model.state_dict(),
-            'optimizer': optimizer.state_dict()
+            "epoch": epoch + 1,
+            "state_dict": model.state_dict(),
+            "optimizer": optimizer.state_dict()
         }
 
-        torch.save(checkpoint, os.path.join(save_model_path, pth_name))
-        print("saved the model " + save_model_path)
-
-
+        torch.save(checkpoint, os.path.join(MODEL_PATH, PTH_NAME))
+        print(f"saved the model {epoch_loss}")
 
 
 def main():
-    device = "cpu"
+    device = "cuda:0"
+    mean, stdv = retrieve_norms()
     tr = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Resize([224, 224])
+        transforms.Resize([224, 224]),
+        transforms.Normalize(mean=mean, std=stdv)
     ])
 
-    train_data = custom_dataset("train", transforms=tr)
-    val_data = custom_dataset("val", transforms= tr)
+    train_data = TrafficSignDataset("train", transforms=tr)
+    val_data = TrafficSignDataset("valid", transforms=tr)
 
-    train_loader = DataLoader(
-        train_data,
-        batch_size=4,
-        shuffle=True
-    )
+    train_loader = DataLoader(train_data, batch_size=4, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=2, drop_last=True)
 
-    val_loader = DataLoader(
-        val_data,
-        batch_size=2,
-        drop_last=True
-    )
-
-    model = ExModel().to(device)   # Initialsing an object of the class.
-    optimizer = SGD(model.parameters(), lr=0.5)
+    # model = Resnet(54).to(device)
+    model = VGG16(54).to(device)
+    optimizer = SGD(model.parameters(), lr=0.001)
+    # optimizer = Adam(model.parameters(), lr=0.001)
     loss = nn.CrossEntropyLoss()
 
-    max_epoch = 15
+    max_epoch = 20
+    train(model, train_loader, val_loader, optimizer, loss, max_epoch, device)
 
 
-    train(model, train_loader, val_loader,  optimizer, loss, max_epoch, device)
-    
-    
 if __name__ == "__main__":
     main()
