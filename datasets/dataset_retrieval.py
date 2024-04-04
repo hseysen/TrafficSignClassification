5414d42a3907c31f462638c528717cad6cb2a2e2
hseysen <hesenisgenderli999@gmail.com>
2024-04-04 16:51:00 +0400

    handle test data loading

--- datasets/dataset_retrieval.py
+++ datasets/dataset_retrieval.py
@@ -14,24 +14,46 @@ class TrafficSignDataset(Dataset):
         self.root = root
         self.transforms = transforms
 
-        # Select split
-        self.folder = os.path.join(self.root, self.mode)
-
         # Initialize lists
         self.image_list = []
         self.label_list = []
 
-        # Save class lists
-        self.class_list = os.listdir(self.folder)
-        self.class_list.sort()
+        if self.mode == "test":
+            # Select split (uppercase folder name)
+            self.folder = os.path.join(self.root, self.mode.upper())
 
-        # Loop over every image and add the corresponding entry to image and label lists
-        for class_id in range(len(self.class_list)):
-            for image in os.listdir(os.path.join(self.folder, self.class_list[class_id])):
-                self.image_list.append(os.path.join(self.folder, self.class_list[class_id], image))
+            # Since test images are not structured, initialize class list first
+            self.class_list = []
+            for testimage in os.listdir(self.folder):
+                thisclass = str(int(testimage[:3]))
+                if thisclass not in self.class_list:
+                    self.class_list.append(thisclass)
+            self.class_list.sort()
+            # Parse test images
+            for testimage in os.listdir(self.folder):
+                imagepath = os.path.join(self.folder, testimage)
+                self.image_list.append(imagepath)
+                
+                imagelabl = str(int(testimage[:3]))
+                class_id = self.class_list.index(imagelabl)
                 label = np.zeros(len(self.class_list))
                 label[class_id] = 1.0
                 self.label_list.append(label)
+        else:
+            # Select split
+            self.folder = os.path.join(self.root, self.mode)
+
+            # Save class lists
+            self.class_list = os.listdir(self.folder)
+            self.class_list.sort()
+
+            # Loop over every image and add the corresponding entry to image and label lists
+            for class_id in range(len(self.class_list)):
+                for image in os.listdir(os.path.join(self.folder, self.class_list[class_id])):
+                    self.image_list.append(os.path.join(self.folder, self.class_list[class_id], image))
+                    label = np.zeros(len(self.class_list))
+                    label[class_id] = 1.0
+                    self.label_list.append(label)
 
     def __getitem__(self, index):
         # Retrieve the entry at requested index
